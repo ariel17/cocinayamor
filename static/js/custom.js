316c33c49bdf9b9b4f9e3def256c9b62a844408f
Ariel Gerardo Ríos <ariel.rios@mercadolibre.com>
2018-06-25 00:55:49 -0300

    Fixes some desktops styles and checks before selecting an option.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,18 +1,38 @@
-function configureMultipleSelect(selector, maxItems) {
-    $(selector).change(function() {
-        var options = $(selector + ' option:selected');
+function configureMultipleSelect(selectSelector, buttonSelector,minItems, maxItems) {
+    var optionsSelector = selectSelector + ' option:selected';
+    $(selectSelector).change(function() {
+        var options = $(optionsSelector);
         if (options.length == maxItems + 1) {
             options[maxItems].selected = false;
+        }
+    });
+
+    $(buttonSelector).click(function(event) {
+        var options = $(optionsSelector);
+        if (options.length < minItems) {
+            event.preventDefault();
+            var message;
+            if (minItems != maxItems) {
+                message = "Debe seleccionar entre " + minItems + " y " + maxItems + " opciones.";
+            } else {
+                message = "Debe seleccionar al menos " + minItems;
+                if (minItems == 1) {
+                    message += " opción.";
+                } else {
+                    message += " opciones.";
+                }
+            }
+            alert(message);
         }
     });
 }
 
 $(function() {
-    configureMultipleSelect('#cya-lunch-select', 8);
-    configureMultipleSelect('#cya-dessert-select', 1);
-    configureMultipleSelect('#cya-cazuelas-select', 1);
-    configureMultipleSelect('#cya-fingerfood-select', 1);
-    configureMultipleSelect('#cya-kids-select', 1);
-    configureMultipleSelect('#cya-principal-1-select', 1);
-    configureMultipleSelect('#cya-principal-2-select', 1);
+    configureMultipleSelect('#cya-lunch-select', '#cya-lunch-ok', 4, 8);
+    configureMultipleSelect('#cya-dessert-select', '#cya-dessert-ok', 1, 1);
+    configureMultipleSelect('#cya-cazuelas-select', '#cya-cazuelas-ok', 1, 1);
+    configureMultipleSelect('#cya-fingerfood-select', '#cya-fingerfood-ok', 1, 1);
+    configureMultipleSelect('#cya-kids-select', '#cya-kids-ok', 1, 1);
+    configureMultipleSelect('#cya-principal-1-select', '#cya-principal-1-ok', 1, 1);
+    configureMultipleSelect('#cya-principal-2-select', '#cya-principal-2-ok', 1, 1);
 });
