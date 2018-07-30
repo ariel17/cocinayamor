a5ffa94fece90117f7bb55ffbfb780875ca7828e
Ariel Gerardo Ríos <ariel.rios@mercadolibre.com>
2018-07-30 01:43:39 -0300

    Adds react for Formal Reception #1.

--- static/js/react/formal.js
+++ static/js/react/formal.js
@@ -6,18 +6,6 @@ const formalMenuData = [
 ]
 
 
-class SectionTitle extends React.Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-sm-12 col-md-12 cya-section">
-                    <div className="text-center cya-title">{this.props.text}</div>
-                </div>
-            </div>
-        );
-    }
-}
-
 class Formal extends React.Component {
     render() {
         return (
@@ -25,9 +13,9 @@ class Formal extends React.Component {
                 <NavMenu data={formalMenuData} href="index.html" />
                 <SectionTitle text="Recepci&oacute;n" />
                 <div className="row row-full">
-                    <Option className="col-md-4 cya-formal-reception-1" href="cya-formal-reception-1.html" text="Opci&oacute;n #1" />
-                    <Option className="col-md-4 cya-formal-reception-2" href="cya-formal-reception-1.html" text="Opci&oacute;n #2" />
-                    <Option className="col-md-4 cya-formal-reception-3" href="cya-formal-reception-1.html" text="Opci&oacute;n #3" />
+                    <Option className="col-md-4 cya-formal-reception-1" href="formal-reception-1.html" text="Opci&oacute;n #1" />
+                    <Option className="col-md-4 cya-formal-reception-2" href="formal-reception-2.html" text="Opci&oacute;n #2" />
+                    <Option className="col-md-4 cya-formal-reception-3" href="formal-reception-3.html" text="Opci&oacute;n #3" />
                 </div>
             </div>
         );
---
+++ static/js/react/lib/section.js
@@ -0,0 +1,13 @@
+class SectionTitle extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm-12 col-md-12 cya-section">
+                    <div className="text-center cya-title">{this.props.text}</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
